dedad6a6936e59557226e5aaedf90647d2504dfd
Thed <theodore1seriakov@gmail.com>
2021-12-25 23:34:03 +0600

    Initial commit

---
+++ src/com/company/FileF.java
@@ -0,0 +1,18 @@
+//package com.company;
+//import java.io.*;
+//public class FileF {
+//    File f = new File("test10.txt");
+//    FileOutputStream fos = new FileOutputStream(f);
+//    FileInputStream fis = new FileInputStream(f);
+//
+////    fis.write();
+//
+//    public FileF() throws FileNotFoundException {
+//    }
+//
+//    public FileF(File f, FileOutputStream fos, FileInputStream fis) throws FileNotFoundException {
+//        this.f = f;
+//        this.fos = fos;
+//        this.fis = fis;
+//    }
+//}
